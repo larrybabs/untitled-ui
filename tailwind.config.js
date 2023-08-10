17582dcdc7a663e169c4b72744703a79c4acc382
Samad <seshp@ymail.com>
2023-08-10 14:29:13 +0100

    add custom colors to tailwind config and Inter font to index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#344054',
+        'secondary': '#667085',
+        'dark': '#101828',
+       
+      },
+    },
   },
   plugins: [],
 }
